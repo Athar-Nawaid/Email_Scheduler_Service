36f2c8b97ef1f2665a4e4a475e3722436483139f
Athar Nawaid <a.nawaid98@gmail.com>
2026-01-22 01:36:08 +0530

    Refactor Redis connection to use environment variable

--- Backend/src/redis.ts
+++ Backend/src/redis.ts
@@ -1,6 +1,3 @@
-import IORedis from "ioredis";
+import Redis from "ioredis";
 
-export const redis = new IORedis({
-  host: "127.0.0.1",
-  port: 6379
-});
+export const redis = new Redis(process.env.REDIS_URL!);
